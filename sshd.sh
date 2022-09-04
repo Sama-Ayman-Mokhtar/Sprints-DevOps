353a544081ca97708a7e32ac901a6cf2232d6320
Sama-Ayman_Mokhtar <sama2ayman2@gmail.com>
2022-09-04 17:50:29 +0100

    Task 03: Securing SSH

--- sshd.sh
+++ sshd.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
 
-echo "Please Enter an Integer To Set as SSH Port: "
+# To run:
+# sudo ./sshd.sh
+
+echo "Please Enter an Integer To Set as SSH Port: (run as root)"
 read INT_FROM_USER
 if [[ ${INT_FROM_USER} -lt 1024  && ${INT_FROM_USER} != 22 || ${INT_FROM_USER} -gt 65535 ]]
 then 
-	echo "invalid port number - choose a number between 1024 and 65535 or 22"
+	echo "[Error] Invalid port number - choose a number between 1024 and 65535 or 22"
 else
 	sed -i -e "/Port /c\Port ${INT_FROM_USER}" /etc/ssh/sshd_config
-	echo "[+] Edited the config file"
-	service ssh restart
-	echo "[+] Restarted ssh service"
+	echo "[+] Changed ssh port number"
 fi
+
+sed -i -e "/PermitRootLogin /c\PermitRootLogin no" /etc/ssh/sshd_config
+echo "[+] Disabled root login to system"
+service sshd restart
+echo "[+] Restarted ssh service"
 
